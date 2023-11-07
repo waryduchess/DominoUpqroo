cc757e8ee63972993899edefb6565f1b5feb9162
Almondcurve <nutritivesquid@gmail.com>
2023-11-06 19:59:06 -0500

    generador de ficha
    mezclador de fichas, creacion de la carpeta de molules par aagregar el mezclador

--- domino.js
+++ domino.js
@@ -3,17 +3,26 @@ const rl = require("readline-sync");
 
 var left = [1,2,3,4,5,6];
 var right = [1,2,3,4,5,6];
+var position = ["horizontal","vertical"];
 var fichas = {
-   izquierda: null,
-   derecha: null
+   izquierda: null, //0-6
+   derecha: null,  // 0-6
+   posicion : null
   };
 
 const dominos = [];
 for (var cleft = 0; cleft <= left.length; cleft++) {
-  for (let cright = cleft; cright <= right.length; cright++) {
-    dominos.push({ izquierda: cleft ,derecha: cright });
+  for (let cright = cleft+1; cright <= right.length; cright++) {
+    dominos.push({ izquierda: cleft ,derecha: cright, posicion:position[0]});
   }
 }
+dominos.push({izquierda:0, derecha:0, posicion:position[1]})
+dominos.push({izquierda:1, derecha:1, posicion:position[1]})
+dominos.push({izquierda:2, derecha:2, posicion:position[1]})
+dominos.push({izquierda:3, derecha:3, posicion:position[1]})
+dominos.push({izquierda:4, derecha:4, posicion:position[1]})
+dominos.push({izquierda:5, derecha:5, posicion:position[1]})
+dominos.push({izquierda:6, derecha:6, posicion:position[1]})
 console.log("las fichas con",dominos);
 const players = rl.question("¿cuantos jugadores seran?", {});
 const totalfichas = players * 5;
@@ -32,5 +41,5 @@ for (var cTfichas = 0; cTfichas < 5; cTfichas++) {
   console.log(dominos)
   var well = [];
  
-  well.push(dominos.shift());
-  console.log(well);
+//well.push(dominos.shift());
+  //console.log(well);
